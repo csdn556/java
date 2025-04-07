3048df9fcbb0716af8022ab183396f9ceb99e64d
jiangzhizhi <1512988489@qq.com>
2025-04-07 10:37:57 +0800

    更新摩尔投票算法

--- src/majorityElement.java
+++ src/majorityElement.java
@@ -1,19 +1,27 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-        int m = 0;
-        int count = 0;
+public class majorityElement {
+    public static int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
 
-        for (int n : nums) {
+        // 1. Find the candidate for majority element  
+        for (int i = 1; i < nums.length; i++) {
             if (count == 0) {
-                m = n;
-                count += 1;
-            } else if (m == n) {
-                count += 1;
+                candidate = nums[i];
+                count = 1;
+            } else if (nums[i] == candidate) {
+                count++;
             } else {
-                count -= 1;
+                count--;
             }
         }
 
-        return m;
+        // Since the problem guarantees a majority element, we don't need to check the candidate  
+        return candidate;
     }
-}
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 3}; // Example input  
+        int result = majorityElement(nums);
+        System.out.println("Majority Element: " + result); // Output: 3  
+    }
+}  
