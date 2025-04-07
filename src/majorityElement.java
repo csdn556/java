24f3095bf03710ca2dc36e0bd1db9d5b8a474170
jiangzhizhi <1512988489@qq.com>
2025-04-07 10:28:13 +0800

    新增摩尔投票算法

--- src/majorityElement.java
+++ src/majorityElement.java
@@ -1,2 +1,19 @@
-public class majorityElement {
+class Solution {
+    public int majorityElement(int[] nums) {
+        int m = 0;
+        int count = 0;
+
+        for (int n : nums) {
+            if (count == 0) {
+                m = n;
+                count += 1;
+            } else if (m == n) {
+                count += 1;
+            } else {
+                count -= 1;
+            }
+        }
+
+        return m;
+    }
 }
